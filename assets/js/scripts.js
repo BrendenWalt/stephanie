675cc71c26c8a7a90f44758104bf96a3cdbe6876
Brenden W <brendenwalters42@gmail.com>
2019-09-10 21:39:03 -0400

    Scroll to top button

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -3,6 +3,7 @@ window.onload = function() {
   const hamburger = document.querySelector('.ham-container');
   const navLinks = document.querySelector('.navbar');
   const navLink = navLinks.querySelectorAll('li');
+  const scrollTop = document.querySelector('#scroll-to-top');
 
 
   function onHamClick(e) {
@@ -23,7 +24,26 @@ window.onload = function() {
     });
   }
 
+  function onTopClick(e) {
+    e.preventDefault();
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  }
+
+  function onWindowScroll() {
+    console.log(window.pageYOffset);
+    if(window.pageYOffset > 600) {
+      scrollTop.classList.add('active');
+    } else {
+      scrollTop.classList.remove('active');
+    }
+  }
+
   hamburger.addEventListener('click', onHamClick);
+  scrollTop.addEventListener('click', onTopClick);
+  window.addEventListener('scroll', onWindowScroll)
   
   for( var i = 0; i < navLink.length; i++) {
     navLink[i].addEventListener('click', onNavLinkClick);
