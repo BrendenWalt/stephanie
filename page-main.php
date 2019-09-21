8e9f55557172591364650ff379097e73c72cd9e4
Brenden W <brendenwalters42@gmail.com>
2019-09-21 14:57:55 -0400

    Video custom fields setup

--- page-main.php
+++ page-main.php
@@ -24,6 +24,11 @@ $about_image                             = get_field('about_image');
 $special_skills                          = get_field('special_skills');
 $download_resume_link                    = get_field('download_resume_link');
 
+// VIDEOS
+$featured_video                          = get_field('featured_video');
+$video_2                                 = get_field('video_2');
+$video_3                                 = get_field('video_3');
+
 get_header();
 ?>
 
@@ -211,13 +216,13 @@ get_header();
         <div class="container">
           <div class="video-grid">
             <div class="video-item">
-              <img src="https://placekitten.com/1920/1080" alt="">
+              <iframe width="100%" height="100%" src="<?php echo($featured_video); ?>" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" style="height: 100%;" allowfullscreen></iframe>
             </div>
             <div class="video-item">
-              <img src="https://placekitten.com/1920/1080" alt="">
+              <iframe width="100%" height="100%" src="<?php echo($video_2); ?>" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" style="height: 100%;" allowfullscreen></iframe>
             </div>
             <div class="video-item">
-              <img src="https://placekitten.com/1920/1080" alt="">
+              <iframe width="100%" height="100%" src="<?php echo($video_3); ?>" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" style="height: 100%;" allowfullscreen></iframe>
             </div>
           </div>
         </div>
