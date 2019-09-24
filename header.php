d56a80f2c58c3b97ec8b7c879ec2feedd48780ea
Brenden W <brendenwalters42@gmail.com>
2019-09-23 20:52:59 -0400

    Added archive pages for videos and photos

--- header.php
+++ header.php
@@ -33,7 +33,11 @@
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'stephanie-vanalstine' ); ?></a>
 
 	<header>
+		<?php if(is_page_template('page-main.php')) { ?>
 		<div class="menu-wrap">
+			<div class="menu-logo">
+
+			</div>
 			<div class="menu-container">
 				<?php 
 					wp_nav_menu( array(
@@ -51,6 +55,29 @@
 				<span></span>
 			</div>
 		</div>
+		<?php } else { ?>
+			<div class="menu-wrap alt-page-menu">
+				<div class="menu-logo">
+
+				</div>
+				<div class="menu-container">
+					<?php 
+						wp_nav_menu( array(
+							'theme_location'	=> 'primary',
+							'container'				=> 'nav',
+							'container_class'	=> 'navbar-collapse navbar closed',
+							'menu_class'			=> 'nav navbar-nav navbar-right'
+						))
+					?>
+				</div>
+				<div id="nav-icon3" class="ham-container">
+					<span></span>
+					<span></span>
+					<span></span>
+					<span></span>
+				</div>
+			</div>
+		<?php } ?>
 		
 		
 	</header>
