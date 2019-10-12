3ebae5cd9c9f2b177629ce68444abd3ce01f0249
Brenden W <brendenwalters42@gmail.com>
2019-10-12 17:48:20 -0400

    photo archive page styles

--- archive-photos.php
+++ archive-photos.php
@@ -11,38 +11,48 @@ get_header();
 ?>
 
 	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
+		<main id="main" class="site-main post-archive">
 
-		<?php if ( have_posts() ) : ?>
-
-			<header class="page-header">
-				<?php
-				the_archive_title( '<h1 class="page-title">', '</h1>' );
+			<!-- Page header -->
+			<section class="hero">
+				<div class="hero-body">
+					<div class="container">
+					<?php
+				// the_archive_title( '<h1 class="page-title">', '</h1>' );
 				?>
-				<h2>Gallery</h2>
-			</header><!-- .page-header -->
+						<h1>Photos<h1>
+					</div>
+				</div>
+			</section>
 
-			<?php
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
+			<div class="container">
 
-				/*
-				 * Include the Post-Type-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_type() );
+			<?php if ( have_posts() ) : ?>
 
-			endwhile;
+				<?php
+				/* Start the Loop */
+				while ( have_posts() ) :
+					the_post();
 
-		else :
+					/*
+					* Include the Post-Type-specific template for the content.
+					* If you want to override this in a child theme, then include a file
+					* called content-___.php (where ___ is the Post Type name) and that will be used instead.
+					*/
+					get_template_part( 'template-parts/content', 'archive-photos', get_post_type() );
 
-			get_template_part( 'template-parts/content', 'none' );
+				endwhile;
 
-		endif;
-		?>
-			
+			else :
+
+				get_template_part( 'template-parts/content', 'none' );
+
+			endif;
+			?>
+
+			</div>
+		
+
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
